781fff2450491653891a716e1d26906fad265f27
mohamed kamal <mohamedjop0100@gmail.com>
2025-10-22 20:04:34 +0300

    إصلاح fetch في صفحة sale

--- app/sale/page.jsx
+++ app/sale/page.jsx
@@ -1,27 +1,34 @@
 import Card from '../componands/cards/card'
 import Head from '../componands/pagesHead/head'
 import BlackLine from '../componands/homePageComponands/more/blackLine'
+
 export const revalidate = 2592000; 
 
-export default async function SalePage(){
+export default async function SalePage() {
     const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
-    const res = await fetch(`${baseUrl}/api/data/sale` , {
-        next: { revalidate: 2592000 },
-    })
-    
-    if (!res.ok) throw new Error("Failed to fetch best sellers");
+    let data = [];
 
-    const data = await res.json()
+    try {
+        const res = await fetch(`${baseUrl}/api/data/sale`, {
+            next: { revalidate: 2592000 },
+        });
 
-    return(
+        if (!res.ok) throw new Error("Failed to fetch best sellers");
+
+        data = await res.json();
+    } catch (error) {
+        console.error("❌ Error fetching sale data:", error.message);
+    }
+
+    return (
         <>
             <Head pageName={'sale'} />
             <section>
                 <BlackLine title={'Sale'} />
-                <div className=" container m-auto relative">
-                    <Card data={data} sale={true} />                   
+                <div className="container m-auto relative">
+                    <Card data={data} sale={true} />
                 </div>
             </section>
         </>
-    )
+    );
 }
