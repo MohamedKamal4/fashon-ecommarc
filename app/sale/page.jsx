4f17b52bf414d5b310d89319dab679a581fc74a0
mohamed kamal <mohamedjop0100@gmail.com>
2025-10-23 07:26:34 +0300

    Add collections API route for Vercel build

--- app/sale/page.jsx
+++ app/sale/page.jsx
@@ -1,13 +1,22 @@
+'use client'
 import Card from '../componands/cards/card'
 import Head from '../componands/pagesHead/head'
 import BlackLine from '../componands/homePageComponands/more/blackLine'
+import { useEffect, useState } from 'react';
 
-export default async function SalePage() {
-    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
-    const res = await fetch(`${baseUrl}/api/data/sale` , {
-        next: { revalidate: 2592000 }
-    });
-    const data = await res.json();
+export default function SalePage() {
+    const [data , setData] = useState([])
+    useEffect(() => {
+        const fetchData = async () => {
+            const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+            const res = await fetch(`${baseUrl}/api/data/sale` , {
+                next: { revalidate: 2592000 }
+            });
+            const data = await res.json();
+            setData(data)
+        }
+        fetchData()
+    },[])
     return (
         <>
             <Head pageName={'sale'} />
