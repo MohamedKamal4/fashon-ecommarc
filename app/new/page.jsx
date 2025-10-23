c5a1f0e5c999326c731814f4956f2543a7e294bf
mohamed kamal <mohamedjop0100@gmail.com>
2025-10-23 07:40:27 +0300

    Add collections API route for Vercel build

--- app/new/page.jsx
+++ app/new/page.jsx
@@ -3,7 +3,8 @@ import { useEffect, useState } from "react";
 import BtnList from "./btnList";
 
 export default function New() {
-    const [data , setData] = useState([])  
+    const [data , setData] = useState([])
+    const [AllProducts , setAllProducts] = useState([])
     const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
 
     useEffect(() => {
@@ -11,8 +12,9 @@ export default function New() {
         .then((res) => res.json)
         .then((res) => {
           setData(res)
+          setAllProducts([...res.pants , ...res.jackets , ...res.hoodies])
         })
     },[])
 
-  return <BtnList data={data} AllProducts={AllProducts} />;
+  return <BtnList data={data} AllProducts={AllProducts} />
 }
