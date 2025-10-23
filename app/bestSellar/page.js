c32770244ecfa1b3dfbc093fa6a6b3aca70beab7
mohamed kamal <mohamedjop0100@gmail.com>
2025-10-23 07:26:59 +0300

    Add collections API route for Vercel build

--- app/bestSellar/page.js
+++ app/bestSellar/page.js
@@ -1,14 +1,24 @@
+'use client'
 import Card from "../componands/cards/card";
 import Head from "../componands/pagesHead/head";
 import BlackLine from "../componands/homePageComponands/more/blackLine";
+import { useEffect, useState } from "react";
 export const revalidate = 2592000;
 
-export default async function BestSellar() {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
-  const res = await fetch(`${baseUrl}/api/data/bestSellers` , {
-    next: { revalidate: 2592000 },
-  })
-  const data = await res.json() 
+export default function BestSellar() {
+  const [data , setData] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+      const res = await fetch(`${baseUrl}/api/data/bestSellers` , {
+        next: { revalidate: 2592000 },
+      })
+      const data = await res.json() 
+      setData(data)
+    }
+    fetchData()
+  },[])
 
   return (
     <>
